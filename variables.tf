40f2aae2793b17f3d56c3b3bf88e8959891f0f08
TaeAhmed <taeahmed@outlook.com>
2024-09-21 16:36:18 +0300

    completed 1,2

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "public_subnet_cidrs" {
+default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+variable "private_subnet_cidrs" {
+default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
