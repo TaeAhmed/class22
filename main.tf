814c8abcacc82b86d4218efcc6d56a178e7bffba
TaeAhmed <taeahmed@outlook.com>
2024-09-21 16:41:08 +0300

    completed 1,2

--- main.tf
+++ main.tf
@@ -16,13 +16,63 @@ module "vpc" {
     vpc_cidr = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet" {
-  vpc_id     = "${module.vpc.vpc_id}"
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "public" {
+    count = 3
+    vpc_id     = "${module.vpc.vpc_id}"
+    cidr_block = element(var.public_subnet_cidrs, count.index)
+    map_public_ip_on_launch = true
+}
 
-  tags = {
-    Name = "Main"
-  }
+resource "aws_subnet" "private" {
+    count = 3
+    vpc_id     = "${module.vpc.vpc_id}"
+    cidr_block = element(var.private_subnet_cidrs, count.index)
+    map_public_ip_on_launch = false
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_nat_gateway" "natgw" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public[0].id
+}
+
+resource "aws_eip" "nat_eip" {
+    vpc = true
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_route_table" "private" {
+    vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_route" "public_route" {
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route" "private_route" {
+    route_table_id = aws_route_table.private.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+}
+
+resource "aws_route_table_association" "public" {
+    count = 3
+    subnet_id = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+    count = 3
+    subnet_id = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private.id
 }
 
 module "web-server" {
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "${var.vpc_cidr}"
-  instance_tenancy = "${var.tenancy}"
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = var.tenancy
 
   tags = {
     Name = "main"
